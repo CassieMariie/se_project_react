d5a0eed51481678fd0eaa272d85eabcb95ad0ed1
CassieMarie <cassie.mpt@gmail.com>
2025-01-28 12:08:13 -0800

    updated changes

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.svg";
+
+function Header({ handleActiveModal, weatherData }) {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+  return (
+    <header className="header">
+      <img src={logo} className="header__logo" />
+      <p className="header__date-location">
+        {currentDate}, {weatherData.city}
+      </p>
+      <button
+        onClick={handleActiveModal}
+        type="button"
+        className="header__add-clths-button"
+      >
+        + Add Clothes
+      </button>
+      <div className="header__user-container">
+        <p className="header__username">Terrance Tegegne</p>
+        <img src={avatar} alt="Terrance Tegegne" className="header__avatar" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
